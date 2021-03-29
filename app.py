eb539dd13556dbde81c15a10f39bcc54605afa22
Benjamin Bramanti <bbramanti@gmail.com>
2021-03-29 13:27:31 -0500

    loading initial project files to repo

---
+++ app.py
@@ -0,0 +1,186 @@
+import pandas as pd
+import numpy as np
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Output, Input
+import plotly.express as px
+import random
+import logging
+
+# read dataset into pandas dataframe from csv file
+data = pd.read_csv("./data/ytd_timberwolves_player_boxscore.csv")
+# convert DATE field into datetime field (necessary to work with line charts)
+data["DATE"] = pd.to_datetime(data["DATE"], format="%m/%d/%Y")
+# add % into data
+data["FG-PCT"] = data["FGM"]/data["FGA"]
+data["3-PCT"] = data["3PM"]/data["3PA"]
+
+# initialize app
+app = dash.Dash(__name__)
+# server variable needed for procfile
+server = app.server
+
+# build layout (css contained in /assets)
+app.layout = html.Div(
+    id = "outer",
+    children = [
+        html.Div(
+            id = "inner-1",
+            children = [
+                html.Img(
+                    id = "logo",
+                    src = "https://cdn.freebiesupply.com/images/large/2x/minnesota-timberwolves-logo-transparent.png"
+                )
+            ]
+        ),
+        html.Div(
+            id = "inner-2",
+            children = [
+                html.H1(className="chart-title", children=["PTS / AST / REB"]),
+                dcc.Dropdown(
+                    id = "player-filter",
+                    options = [
+                        {"label": player, "value": player}
+                        for player in np.sort(data.PLAYER.unique())
+                    ],
+                    value = "Anthony Edwards",
+                    clearable = False,
+                    className = "dropdown"
+                ),
+                dcc.Graph(id="points-chart"),
+                dcc.Graph(id="assists-chart"),
+                dcc.Graph(id="rebounds-chart"),
+                html.H1(className="chart-title", children=["MINUTES & +/-"]),
+                dcc.Graph(
+                    id="team-min-chart",
+                    figure = px.line(
+                        data,
+                        x="DATE",
+                        y="MIN",
+                        color="PLAYER",
+                        color_discrete_sequence=px.colors.qualitative.Light24,
+                        title='Minutes (YTD)',
+                        custom_data = ["MATCHUP", "W/L", "PLAYER"],
+                        template = 'plotly_dark'
+                    ).for_each_trace(
+                        # any player averaging less than 25 minutes a game, do not show their trace (line)
+                        lambda trace: trace.update(
+                            visible='legendonly',
+                            hovertemplate = '<b>%{customdata[2]}</b>' +
+                                            '<br><b>Matchup</b>: %{customdata[0]}<br>' +
+                                            '<b>Date</b>: %{x}<br>'+
+                                            '<b>Win/Loss</b>: %{customdata[1]}<br>' +
+                                            '<b>Minutes</b>: %{y}<br>' + '<extra></extra>',
+                            mode='markers+lines') if np.mean(trace.y) < 25 else (trace.update(
+                                visible=True,
+                                mode='markers+lines',
+                                hovertemplate = '<b>%{customdata[2]}</b>' +
+                                                '<br><b>Matchup</b>: %{customdata[0]}<br>' +
+                                                '<b>Date</b>: %{x}<br>'+
+                                                '<b>Win/Loss</b>: %{customdata[1]}<br>' +
+                                                '<b>Minutes</b>: %{y}<br>' + '<extra></extra>',
+                            )
+                            )
+                    )
+                ),
+                dcc.Graph(
+                    id="team-plus-minus-chart",
+                    figure = px.bar(
+                        data,
+                        x="DATE",
+                        y="MIN",
+                        color="PLUS-MINUS",
+                        color_continuous_scale=["red", "green"],
+                        color_discrete_sequence=px.colors.qualitative.Light24,
+                        title='Minutes vs. +/- (YTD)',
+                        custom_data = ["MATCHUP", "W/L", "PLAYER", "PLUS-MINUS"],
+                        template = 'plotly_dark'
+                    ).for_each_trace(
+                        # any player averaging less than 0 +/- a game, do not show their trace (line)
+                        lambda trace: trace.update(
+                            visible='legendonly',
+                            hovertemplate = '<b>%{customdata[2]}</b>' +
+                                            '<br><b>Matchup</b>: %{customdata[0]}<br>' +
+                                            '<b>Date</b>: %{x}<br>'+
+                                            '<b>Win/Loss</b>: %{customdata[1]}<br>' +
+                                            '<b>Minutes</b>: %{y}<br>' +
+                                            '<b>+/-</b>: %{customdata[3]}<br>' + '<extra></extra>',
+                            ) if np.mean(trace.y) < -3 else (trace.update(
+                                visible=True,
+                                hovertemplate = '<b>%{customdata[2]}</b>' +
+                                                '<br><b>Matchup</b>: %{customdata[0]}<br>' +
+                                                '<b>Date</b>: %{x}<br>'+
+                                                '<b>Win/Loss</b>: %{customdata[1]}<br>' +
+                                                '<b>Minutes</b>: %{y}<br>' +
+                                                '<b>+/-</b>: %{customdata[3]}<br>' + '<extra></extra>',
+                            )
+                            )
+                    )
+                )
+            ]
+        )
+    ]
+)
+
+@app.callback(
+    [Output("points-chart", "figure"), Output("assists-chart", "figure"), Output("rebounds-chart", "figure")],
+    Input("player-filter", "value")
+)
+def update_charts(player):
+    filtered_data = data[data["PLAYER"] == player]
+    pts_fig = px.line(
+        filtered_data,
+        x="DATE",
+        y="PTS",
+        title='Points (YTD)',
+        custom_data = ["MATCHUP", "W/L"],
+        template = 'plotly_dark'
+    )
+    pts_fig.update_traces(
+        mode='markers+lines',
+        line_color="#78be20",
+        hovertemplate = '<b>' + player + '</b>' +
+                        '<br><b>Matchup</b>: %{customdata[0]}<br>' +
+                        '<b>Date</b>: %{x}<br>'+
+                        '<b>Win/Loss</b>: %{customdata[1]}<br>' +
+                        '<b>Points</b>: %{y}<br>' + '<extra></extra>'
+    )
+    ast_fig = px.line(
+        filtered_data,
+        x="DATE",
+        y="AST",
+        title='Assists (YTD)',
+        custom_data = ["MATCHUP", "W/L"],
+        template = 'plotly_dark'
+    )
+    ast_fig.update_traces(
+        mode='markers+lines',
+        line_color="#78be20",
+        hovertemplate = '<b>' + player + '</b>' +
+                        '<br><b>Matchup</b>: %{customdata[0]}<br>' +
+                        '<b>Date</b>: %{x}<br>'+
+                        '<b>Win/Loss</b>: %{customdata[1]}<br>' +
+                        '<b>Assists</b>: %{y}<br>' + '<extra></extra>'
+    )
+    reb_fig = px.line(
+        filtered_data,
+        x="DATE",
+        y="REB",
+        title='Rebounds (YTD)',
+        custom_data = ["MATCHUP", "W/L"],
+        template = 'plotly_dark'
+    )
+    reb_fig.update_traces(
+        mode='markers+lines',
+        line_color="#78be20",
+        hovertemplate = '<b>' + player + '</b>' +
+                        '<br><b>Matchup</b>: %{customdata[0]}<br>' +
+                        '<b>Date</b>: %{x}<br>'+
+                        '<b>Win/Loss</b>: %{customdata[1]}<br>' +
+                        '<b>Rebounds</b>: %{y}<br>' + '<extra></extra>'
+    )
+    return pts_fig, ast_fig, reb_fig
+
+if __name__ == "__main__":
+    app.run_server(debug=False)
---
+++ refresh_data.py
@@ -0,0 +1,126 @@
+import pandas as pd
+import time
+import logging
+import datetime
+from nba_api.stats.static import teams
+from nba_api.stats.endpoints import boxscoretraditionalv2, teamgamelog
+from dateutil.parser import parse
+
+# configure logger
+# logging.basicConfig(level=logging.INFO)
+
+# log to signal beginning of function execution
+logging.info("BEGINNING REFRESH_DATA JOB AT {}".format(datetime.datetime.now()))
+
+# read in current state of both csv files
+# GAME_ID field may have leading zeroes, so read in as string
+curr_games_pulled = pd.read_csv('./data/ytd_timberwolves_games_pulled.csv', dtype={'GAME_ID':str})
+curr_player_boxscore = pd.read_csv('./data/ytd_timberwolves_player_boxscore.csv')
+
+# get all teams data
+teams_data = teams.get_teams()
+
+# get Minnesota Timberwolves Team ID
+minnesota_timberwolves_id = [team for team in teams_data if team['abbreviation'] == 'MIN'][0]['id']
+
+# using Team ID pull all games from 2020-2021 Season
+timberwolves_games_2020_2021 = teamgamelog.TeamGameLog(
+    team_id = minnesota_timberwolves_id,
+    season = '2020'
+).get_data_frames()[0]
+
+# select only neccessary data
+timberwolves_games_2020_2021 = timberwolves_games_2020_2021[['Game_ID', 'GAME_DATE', 'MATCHUP', 'WL']]
+
+# fix column names
+timberwolves_games_2020_2021.rename(
+    columns={
+        'Team_ID': "TEAM_ID",
+        'Game_ID': 'GAME_ID'
+    },
+    inplace=True
+)
+
+# change date column format from MAR 15, 2021 -> 3/15/2021
+timberwolves_games_2020_2021['GAME_DATE'] = timberwolves_games_2020_2021.apply(lambda row : parse(row['GAME_DATE']).strftime('%m/%d/%Y'), axis = 1)
+
+# sort by game date instead of game date because date would require parsing back to datetime object
+timberwolves_games_2020_2021.sort_values(by=['GAME_ID'], inplace=True)
+
+# remove all records from timberwolves_games_2020_2021 that exist in curr_games_pulled
+
+timberwolves_games_2020_2021 = pd.concat([timberwolves_games_2020_2021,curr_games_pulled]).drop_duplicates(keep=False)
+logging.info("pulling {} new games".format(len(timberwolves_games_2020_2021)))
+
+if not timberwolves_games_2020_2021.empty:
+
+    # create Dataframe to hold BoxScore data
+    boxscores = pd.DataFrame()
+
+    # pull each game and append results to dataframe
+    for index, game in timberwolves_games_2020_2021.iterrows():
+        logging.info("pulling {} from {} with id {}".format(game['MATCHUP'], game['GAME_DATE'], game['GAME_ID']))
+        game_instance = boxscoretraditionalv2.BoxScoreTraditionalV2(game_id = game['GAME_ID']).get_data_frames()[0]
+        boxscores = boxscores.append(game_instance)
+        time.sleep(5)
+
+    # remove any records for the opposing team, or players who did not log any minutes
+    boxscores = boxscores[
+        (boxscores['TEAM_ID'] == minnesota_timberwolves_id) &
+        (boxscores['MIN'].isnull() == False)
+    ]
+
+    # adjust MIN column -> 23:05 to 23, 23:55 to 24
+    def fix_minutes_played(row):
+        minutes = int(row['MIN'].split(":")[0])
+        seconds = int(row['MIN'].split(":")[1])
+        seconds += (minutes * 60)
+        return round(seconds / 60)
+
+    boxscores['MIN'] = boxscores.apply(lambda row : fix_minutes_played(row), axis = 1)
+
+    # merge BoxScore with Games data to get date/matchup/win-loss
+    boxscores = boxscores.merge(timberwolves_games_2020_2021, on='GAME_ID', how='left')
+
+    # drop columns that are no longer needed
+    boxscores.drop(
+        ['GAME_ID', 'TEAM_ID', 'TEAM_CITY', 'PLAYER_ID', 'START_POSITION', 'COMMENT', 'FG_PCT', 'FG3_PCT', 'FT_PCT'],
+        axis=1,
+        inplace=True
+    )
+
+    # fix column header names
+    boxscores.rename(
+        columns={
+            'PLAYER_NAME': 'PLAYER',
+            'TEAM_ABBREVIATION': 'TEAM',
+            'GAME_DATE': 'DATE',
+            'WL': 'W/L',
+            'FG3M': '3PM',
+            'FG3A': '3PA',
+            'TO': 'TOV',
+            'PLUS_MINUS': 'PLUS-MINUS'
+        },
+        inplace=True
+    )
+
+    # convert float columns to int
+    filter_columns = ['PTS', 'FGM', 'FGA', '3PM', '3PA', 'FTM', 'FTA', 'OREB', 'DREB', 'REB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PLUS-MINUS']
+    boxscores[filter_columns] = boxscores[filter_columns].astype(int)
+
+    # fix ordering of columns
+    column_names = [
+        'PLAYER', 'TEAM', 'DATE', 'MATCHUP', 'W/L',
+        'MIN', 'PTS', 'FGM', 'FGA', '3PM', '3PA',
+        'FTM', 'FTA', 'OREB', 'DREB', 'REB', 'AST', 'STL','BLK',
+        'TOV', 'PF', 'PLUS-MINUS'
+    ]
+
+    boxscores = boxscores.reindex(columns=column_names)
+
+    # append and save files
+    pd.concat([curr_games_pulled,timberwolves_games_2020_2021]).to_csv("./data/ytd_timberwolves_games_pulled.csv", index=False)
+    pd.concat([curr_player_boxscore,boxscores]).to_csv("./data/ytd_timberwolves_player_boxscore.csv", index=False)
+
+# log to signal end of function execution
+logging.info("ENDING REFRESH_DATA JOB AT {}".format(datetime.datetime.now()))
