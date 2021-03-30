fe61a07022c9bb833f88e8fea84e5faedb219c5f
Benjamin Bramanti <bbramanti@gmail.com>
2021-03-30 10:36:15 -0500

    add commenting to refresh script to identify where error takes place

--- refresh_data.py
+++ refresh_data.py
@@ -15,7 +15,9 @@ logging.info("BEGINNING REFRESH_DATA JOB AT {}".format(datetime.datetime.now()))
 # read in current state of both csv files
 # GAME_ID field may have leading zeroes, so read in as string
 curr_games_pulled = pd.read_csv('./data/ytd_timberwolves_games_pulled.csv', dtype={'GAME_ID':str})
+logging.info("num records in games pulled: ".format(len(curr_games_pulled)))
 curr_player_boxscore = pd.read_csv('./data/ytd_timberwolves_player_boxscore.csv')
+logging.info("num records in player boxscore: ".format(len(curr_games_pulled)))
 
 # get all teams data
 teams_data = teams.get_teams()
